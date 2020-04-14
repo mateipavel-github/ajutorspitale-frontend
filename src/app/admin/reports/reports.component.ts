8f811c8de5cf1e4e64cfac9733bcb79e5ca1bacf
Matei Pavel <matei+github@konk-media.com>
2020-04-14 16:55:23 +0300

    Fix .csv filename for exports

--- src/app/admin/reports/reports.component.ts
+++ src/app/admin/reports/reports.component.ts
@@ -25,12 +25,12 @@ export class ReportsComponent implements OnInit {
     report.loading = true;
     this.dataService.downloadCSV(report.url).subscribe( (data: any) => {
       report.loading = false;
-      const blob = new Blob([data], { type: 'text/csv' });
+      const blob = new Blob([data], { type: 'text/csv;charset=utf-8' });
       const d = new Date();
       const element = document.createElement('a');
       element.href = URL.createObjectURL(blob);
       element.download = report.name + ' ' + d.getDay() + '-' + d.getMonth() + '-'
-        + d.getFullYear() + '-' + d.getHours() + '-' + d.getMinutes();
+        + d.getFullYear() + '-' + d.getHours() + '-' + d.getMinutes() + '.csv';
       document.body.appendChild(element);
       element.click();
     }, error => {
